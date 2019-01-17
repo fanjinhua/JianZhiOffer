2ae413fd09fb3f10d2b5d2255e746830febd4d03
Jinhua Fan <fanyiyia@gmail.com>
2019-01-17 19:33:18 +0800

    Add project files.

---
+++ JianZhiOffer/q01.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+	bool Find(int target, vector<vector<int> > array) {
+		int r = 0;
+		int lenr = array.size();
+		int c = array[0].size() - 1;
+
+		while (r < lenr && c >= 0)
+		{
+			if (target > array[r][c])
+				++r;
+			else if (target < array[r][c])
+				--c;
+			else
+				return true;
+		}
+		return false;
+	}
+};
